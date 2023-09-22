617a34b272b0f2dc8a436c7b08fa3a5e9bd244d5
JoyBoyCr7 <Rnyongkah@gmail.com>
2023-09-21 20:38:24 -0500

    change

--- src/components/projects.js
+++ src/components/projects.js
@@ -3,12 +3,12 @@ function Projects(){
     return <div className='projects'>
         <div>
             <img src={require('../images/pthree.png')}/>
-            <p>The Cheeseapp project was created to demonstrate my ability to use React. It also incourperates full CRUD (Create, Read, Update, Delete) functionality.</p>
+            <p>The Cheeseapp project was created to demonstrate my ability to use React. It also incorporates full CRUD (Create, Read, Update, Delete) functionality.</p>
             <a href='https://cheese-frontend-zeta.vercel.app/'>Visit this project</a>
         </div>
         <div>
             <img src={require('../images/pone.png')}/>
-            <p>The NBA Trivia app was created to test the user's knowlendge of basketball history. JQuery was the main tool used to create this project. </p>
+            <p>The NBA Trivia app was created to test the user's knowledge of basketball history. JQuery was the main tool used to create this application.</p>
             <a href='https://sportsflash.onrender.com/'>Visit this project</a>
             </div>
     </div>
