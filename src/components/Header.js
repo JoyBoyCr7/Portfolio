f968e610b148c94fa9c9f4564c2fcac573c12c56
JoyBoyCr7 <Rnyongkah@gmail.com>
2023-10-19 17:46:21 -0500

    send

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
 function Header(){
-    return <div> 
+    return <div style={{display:"flex", justifyContent:"center"}}> 
+        <div>
         <h1>Ronard Nyongkah</h1>
         <h3>Software Engineer</h3>
+        </div>
+        <a href="./Resourses/Ronard\'s-Resume.pdf" style={{display:"flex", alignItems:"center", marginLeft:"500px"}}>Resume</a>
      </div>
     
 }
