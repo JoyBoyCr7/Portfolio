3662804beb18a2d8184c93293df0a759ede5ea10
JoyBoyCr7 <Rnyongkah@gmail.com>
2023-10-22 13:26:47 -0500

    go

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import resume from "../static/Resume.pdf"
+import resume from "../static/RonardResume.pdf"
 function Header(){
     return <div style={{display:"flex", justifyContent:"space-between", paddingTop:"4rem",}}> 
         <div style={{margin:"0px auto"}}>
