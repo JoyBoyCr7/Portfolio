004a6b99f76e9d725e9857d086724b35f3439357
JoyBoyCr7 <Rnyongkah@gmail.com>
2023-11-05 21:23:36 -0600

    grid

--- src/components/projects.js
+++ src/components/projects.js
@@ -2,12 +2,12 @@ function Projects(){
     return <div id='projects' style={{marginTop:"4rem", marginBottom:"4rem"}}>
         <h3>Projects</h3>
         <div className='allprojects' style={{width:"75%", margin:"0px auto", gridGap:"3rem"}}>
-            <div className='project'>
+            {/* <div className='project'>
                 <img src={require('../images/Capstone.png')} alt='proimg' style={{borderRadius:"5%"}}/>
                 <p>This app is an app that allows the user to keep track of the shows they have watched throughout their lives. It allows the user to document when they completed the show and rate the show on a scale of 1 to 5 stars.</p>
                 <p>Skills used: JavaScript, TypeScript, React, HTML, CSS, Node, Mongo, Express, Vite, Material UI. </p>
                 <p>Visit this <a href='https://top-rated-front.vercel.app/'>project</a> and review the code on <a href='https://github.com/JoyBoyCr7/Top-RatedFront'> GitHub.</a> </p>
-            </div>
+            </div> */}
             <div className='project'>
                 <img src={require('../images/jobbyjob.png')} alt='proimg' style={{borderRadius:"5%"}}/>
                 <p>This app is an app that allows the users to keep track of the jobs they may be appliying to. Users have the ability to give small descriptions of the jobs alongside interest level.</p>
