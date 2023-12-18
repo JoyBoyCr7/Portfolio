e25266b85fb6fe711dd7cc1d2ad4429e4992a95a
JoyBoyCr7 <Rnyongkah@gmail.com>
2023-12-18 11:57:09 -0600

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,14 @@
 import resume from "../static/Ronard's_Resume.pdf"
 function Header(){
-    return <div style={{display:"flex", justifyContent:"space-between", paddingTop:"4rem",}}> 
+    return <div> 
+        <nav style={{display:"flex", justifyContent:"space-around", alignItems:"center" , paddingTop:"3rem",marginBottom:'3rem', transition: "all 400ms"}}>
+            <a href="#about" id="navlink">About</a> <a href="#skills" id="navlink"> Skills</a> <a href="#projects" id="navlink">Projects</a> <a href="#contact" id="navlink">Contact</a>
+            <a  href={resume} target="_blank" > <button className="cvbutton"> Resume</button></a>
+       </nav>
         <div style={{margin:"0px auto"}}>
         <h1>Ronard Nyongkah</h1>
         <h3>Software Engineer</h3>
         </div>
-        <nav style={{display:"flex", justifyContent:"center", alignItems:"center" , marginRight:"3rem", gap:"3rem", transition: "all 400ms"}}>
-            <a href="#about" id="navlink">About</a> <a href="#skills" id="navlink"> Skills</a> <a href="#projects" id="navlink">Projects</a> <a href="#contact" id="navlink">Contact</a>
-            <a  href={resume} target="_blank" > <button className="cvbutton"> Resume</button></a>
-       </nav>
      </div>
 }
 
